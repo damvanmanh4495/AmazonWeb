9dbefc1081e6f8c13ed9bc73610ff6f7b91a0f80
damvanmanh4495 <damvanmanh441995@gmail.com>
2021-05-02 21:58:13 +0700

    add user and mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 import data from "./data.js";
+import userRouter from "./routers/useRouter.js";
 
 const app = express();
+mongoose.connect(process.evn.MONGODB_URL || 'mongodb://localhost/amazona', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
 
 app.get('/api/products', (req, res) => {
     res.send(data.products);
@@ -16,8 +23,14 @@ app.get('/api/products/:id', (req, res) => {
     }
 });
 
+app.use('/api/users', userRouter);
+
 app.get("/", (req, res) => {
     res.send('Server is ready');
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send({message: err.message});
 });
 
 const port = process.env.PORT || 5000;
